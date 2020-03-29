32551646166c4c7372b6b359e064142f4a247660
HDS <jonathan.canales.hds@gmail.com>
2020-03-29 02:19:45 -0500

    Login apariencia implementada

--- app/src/main/java/com/example/pepirelax/SplashScreen.kt
+++ app/src/main/java/com/example/pepirelax/SplashScreen.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import com.example.pepirelax.ui.MainActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
@@ -15,14 +20,13 @@ class SplashScreen : AppCompatActivity() {
 
         handler = Handler()
         handler.postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            val intent = Intent(this, MainActivity::class.java)
+            val p1: Pair<View, String> = Pair.create(logoOpening,"transitionImage")
+            val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this,p1)
+            startActivity(intent,options.toBundle())
             finish()
         }, 2000)
 
     }
 
-    //fun transition(item: Activity){
-
-    //}
 }
